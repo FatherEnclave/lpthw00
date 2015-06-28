d19904ded0b8df5e04e59a9fb8d7533a13f391e8
FatherEnclave <DominicDeRose@gmail.com>
2015-06-28 11:17:15 -0400

    Cleaning up repo
    Deleted filler files, removed empty folders, and daily discipline.

---
+++ ex01a.py
@@ -0,0 +1,10 @@
+print "This is the final log of the fallout shelter 501."
+print "Facility in disaray, all systems failing."
+print "I've done my best to mitigate the damage, stem the loss of life, and restore power."
+print "Only essential systems are currently left, and those too will not be last more than another day."
+print "We were emergency broadcasting until someone sabotaged our comlink system. Now we wait to choke to death on stale air."
+print "Fuck the overseer, he could have prevented this."
+print "I'm leaving the vault, there's nothing left for me here but death and misery."
+print "-FatherEnclave"
+#This is a comment
+#comment comment comment
